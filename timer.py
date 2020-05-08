f08ddce3c20f382ba45a633dbeb2704efb28fd7b
Alvin <alvinrcheng@gmail.com>
2020-05-08 16:06:48 +1000

    refactored into functions

--- timer.py
+++ timer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from time import time, localtime,asctime
+from time import time, localtime,asctime, sleep
 import sys
 import winsound
 import threading
@@ -10,6 +10,45 @@ def alarm_sound():
     global curr_active
     while curr_active:
         winsound.Beep(freq, duration)
+
+#this timer loop might be delayed
+def input_thread():
+    global finished
+    global curr_result
+    global curr_active
+    # print(curr_active)
+    while curr_active:
+        curr_result = input()
+        sleep(1)
+    curr_active = False
+    finished = True
+    print("finished input thread: "+curr_result)
+
+def input_options():
+    global curr_active
+    while curr_active:
+        if(curr_result == "exit"):
+            curr_active = False
+            sys.exit()
+        elif(curr_result == "stop"):
+            curr_active = False
+def input_waiting_options():
+    global curr_active
+    global alarm_time
+    global finished
+    global restart
+    global curr_result
+    while time() < alarm_time:
+        if(curr_result == "exit"):
+            curr_active = False
+            sys.exit()
+        elif(curr_result == "restart"):
+            curr_active = False
+            restart = True
+            #wait for thread to finish
+            while (not finished):
+                pass
+            break
 
 #Beep Defs
 duration = 1000  # milliseconds
@@ -31,7 +70,7 @@ mins = 0
 secs = 0
 while not valid:
     try:
-        result = input("What Interval Do You Want To Use?")
+        result = input("What Interval Do You Want To Use? ")
         #print(result)
     except:
         print("Sorry I Didn't Understand Your Input")
@@ -57,44 +96,38 @@ while not valid:
                     elif (data[x] in ("s","sec","secs","second","seconds")):
                         secs = float(data[x-1])
                     else:
-                        print("Incorrect Format: "+data)
-            if (hours or data or secs):
+                        print("Incorrect Format: "+''.join(data))
+            if (hours or mins or secs):
+                print(hours,mins,secs)
+
                 valid = True
-            
-        # else:
-        #     hours = float(data[0])
-        #     mins = float(data[2])
-        #     secs = float(data[4])
-        #     valid = True
         print(data)
         print(hours,mins,secs)
 
 #Timer Loop
 active = True
+curr_active = True
 while active:
-    print("Timer Started, press ctrl and c at the same time to exit")
-    alarm_time = time() + 3600*hours + 60*mins + secs
-    while time() < alarm_time:
-        #IMPLEMENT A STOP TIMER HERE
-        pass
-    print("ALARM!!! It has been "+result)
-    
+    print("Timer Started, type exit or press ctrl and c at the same time to exit")
+    finished = False
+    restart = False
+    curr_result = ""
     curr_active = True
-    # now threading1 runs regardless of user input
-    threading1 = threading.Thread(target=alarm_sound)
+    #input thread for exiting/stopping timer
+    threading1 = threading.Thread(target=input_thread)
+    threading1.daemon = True
     threading1.start()
-    while curr_active:
-        curr_result = input()
-        if(curr_result == "exit"):
-            curr_active = False
-            sys.exit()
-        elif(curr_result == "stop"):
-            curr_active = False
-    threading1.join()
+    #Waiting Loop
+    alarm_time = time() + 3600*hours + 60*mins + secs
+    input_waiting_options()
+    if (restart):
+        continue
+    print("ALARM!!! It has been "+result)
+    curr_active = True
+    #start timer
+    threading2 = threading.Thread(target=alarm_sound)
+    threading2.start()
+    #if we havent restarted or exited input will still be valid here
+    input_options()
+    threading2.join()
     print("running again")
-
-#implement an exit from active
-        
-# curr_time = time()
-# curr_localtime = time.localtime(curr_time)
-# print(curr_localtime)
