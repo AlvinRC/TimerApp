25bdff5e120565f8669876979d2f7372e3faf71f
Alvin <alvinrcheng@gmail.com>
2020-05-08 19:22:51 +1000

    created blank gui

---
+++ app.py
@@ -0,0 +1,9 @@
+#GUI
+import tkinter as tk
+#file dialog help us pick apps, text helps us display text
+from tkinter import filedialog, Text
+#os allows us to run applications
+import os
+
+root = tk.Tk()
+root.mainloop()
